1177425513ca8023cbaa03234bf6964462a66673
Joel Palmius <joepal1976@hotmail.com>
2018-03-09 09:17:20 +0100

    Revert my ABC solution in favor Aranuvirs.
    This reverts commit 7f2ac3789f64c13f1857127ce144ba42a4345f70.

--- 1_mhapi/namespace.py
+++ 1_mhapi/namespace.py
@@ -9,65 +9,39 @@ from abc import *
 # will print all such accesses to the console prompt
 api_tracing = False;
 
-# The following is an ugly copy/paste, but since python2 and python3 
-# have different syntaxes regarding abstract base classes, this is 
-# the most stable solution
 
-if(sys.version_info >= (3,0)):
+# This is needed because python 2 and 3 do not share any syntax for 
+# creating classes with metaclasses
+def classCompat(parent):
+    class uglyWorkaroundClass(parent):
+        def __new__(self, name):
+            return uglyWorkaroundClass(name)
+    return type.__new__(uglyWorkaroundClass,'uglyWorkaroundClass')
 
-    class NameSpace(ABC):
-    
-        def __init__(self):
-            global api_tracing
-            self.tracing = api_tracing
-            self.trace()
-    
-        # Utility method for printing info about where we are in the code
-        # to the console prompt. In the future we should probably use the
-        # log function instead.
-        def trace(self):
-            if self.tracing:
-                info = dict();
-    
-                stack = inspect.currentframe().f_back
-                info["line_number"] = str(stack.f_lineno)
-                info["caller_name"] = stack.f_globals["__name__"]
-                info["file_name"] = stack.f_globals["__file__"]
-                info["caller_method"] = inspect.stack()[1][3]
-    
-                stack = inspect.stack()
-                info["caller_class"] = str(stack[1][0].f_locals["self"].__class__)
-    
-                print("TRACE {}.{}():{}".format(info["caller_name"], info["caller_method"], info["line_number"]))
 
-else:
+class NameSpace(classCompat(ABCMeta)):
 
-    class NameSpace:
-    
-        __metaclass__ = ABCMeta
+    def __init__(self):
+        global api_tracing
+        self.tracing = api_tracing
+        self.trace()
 
-        def __init__(self):
-            global api_tracing
-            self.tracing = api_tracing
-            self.trace()
-    
-        # Utility method for printing info about where we are in the code
-        # to the console prompt. In the future we should probably use the
-        # log function instead.
-        def trace(self):
-            if self.tracing:
-                info = dict();
-    
-                stack = inspect.currentframe().f_back
-                info["line_number"] = str(stack.f_lineno)
-                info["caller_name"] = stack.f_globals["__name__"]
-                info["file_name"] = stack.f_globals["__file__"]
-                info["caller_method"] = inspect.stack()[1][3]
-    
-                stack = inspect.stack()
-                info["caller_class"] = str(stack[1][0].f_locals["self"].__class__)
-    
-                print("TRACE {}.{}():{}".format(info["caller_name"], info["caller_method"], info["line_number"]))
+    # Utility method for printing info about where we are in the code
+    # to the console prompt. In the future we should probably use the
+    # log function instead.
+    def trace(self):
+        if self.tracing:
+            info = dict();
 
+            stack = inspect.currentframe().f_back
+            info["line_number"] = str(stack.f_lineno)
+            info["caller_name"] = stack.f_globals["__name__"]
+            info["file_name"] = stack.f_globals["__file__"]
+            info["caller_method"] = inspect.stack()[1][3]
+
+            stack = inspect.stack()
+            info["caller_class"] = str(stack[1][0].f_locals["self"].__class__)
+
+            print("TRACE {}.{}():{}".format(info["caller_name"], info["caller_method"], info["line_number"]))
 
 
