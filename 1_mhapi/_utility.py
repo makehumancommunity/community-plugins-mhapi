e528e1e33f3bcc982a93ce687d6f36390138e9c1
Joel Palmius <joepal1976@hotmail.com>
2018-10-17 07:56:02 +0200

    Serialize types and values as strings

--- 1_mhapi/_utility.py
+++ 1_mhapi/_utility.py
@@ -6,6 +6,7 @@ import os
 import io
 import struct
 import inspect
+import array
 
 from .logchannel import LogChannel
 
@@ -35,6 +36,71 @@ class Utility(NameSpace):
             self.hasPyQt = (pkgutil.find_loader("PyQt4") is not None)
 
         self.logChannels = {}
+
+    def getTypeAsString(self, content):
+
+        if isinstance(content, int):
+            return "int"
+        if isinstance(content, float):
+            return "float"
+        if isinstance(content, dict):
+            return "dict"
+        if isinstance(content, list):
+            return "list"
+        if isinstance(content, array.array):
+            return "array(" + content.typecode + ")"
+        if self.isPy3:
+            if isinstance(content, bytes):
+                return "bytes"
+            if isinstance(content, str):
+                return "unicode"
+        else:
+            if isinstance(content, unicode):
+                return "unicode"
+            if isinstance(content, str):
+                return "bytes"
+
+        # None of the listed types matched
+
+        with open("/tmp/missingtype.txt","a") as f:
+            f.write(str(type(content)))
+            f.write("\n")
+
+        return str(type(content))
+
+    def getValueAsString(self, content, newLinesIfComplex=False):
+        if isinstance(content, list) or isinstance(content, array.array):
+            result = "[]"
+            #for value in content:
+            #    if result != "":
+            #        if newLinesIfComplex:
+            #            result = result + ",\n"
+            #        else:
+            #            result = result + ", "
+            #    result = result + self.getValueAsString(value)
+            return result
+        if self.isPy3:
+            if isinstance(content, str):
+                return content
+            if isinstance(content, bytes):
+                try:
+                    val = content.decode("utf-8")
+                except:
+                    import binascii
+                    val = binascii.hexlify(content).decode("utf-8")
+                return val
+        else:
+            if isinstance(content, str):
+                return content
+        if isinstance(content, float):
+            # This is to parry differences in precision in py2 vs py3
+            # Even when using round(), results will differ. Thus we do
+            # a destructive floor instead.
+            precision = 100000.0
+            intContent = int(precision * round(content,8))
+            floatContent = float(intContent) / precision
+            return "{0:.4f}".format(floatContent)
+        return str(content)
 
     def isPySideAvailable(self):
         return self.hasPySide
@@ -88,23 +154,6 @@ class Utility(NameSpace):
                 else:
                     f.write( bytes(content) )
 
-        with open(fnTxt, "a") as f:
-            wasWritten = False
-            if isinstance(content, list):
-                for value in content:
-                    if isinstance(value, bytes):
-                        f.write( value.decode('utf-8') )
-                    else:
-                        f.write( str(value) )
-                wasWritten = True
-            if isinstance(content, bytes) and not wasWritten:
-                f.write( content.decode('utf-8') )
-                wasWritten = True
-            if not wasWritten:
-                f.write( str(content) )
-
-            f.write("\n")
-
     def _py2debugWrite(self, content, fnBin, fnTxt):
         with open(fnBin, "wb") as f:
             wasWritten = False
@@ -121,24 +170,6 @@ class Utility(NameSpace):
             if not wasWritten:
                 f.write( bytes(content) )
 
-        with open(fnTxt, "a") as f:
-            wasWritten = False
-            if isinstance(content, list):
-                for value in content:
-                    if isinstance(value, bytes):
-                        f.write( value.decode('utf-8') )
-                    else:
-                        f.write( str(value) )
-                wasWritten = True
-            if isinstance(content, bytes) and not wasWritten:
-                f.write( content.decode('utf-8') )
-                wasWritten = True
-            if not wasWritten:
-                f.write( str(content) )
-
-            f.write("\n")
-
-
     def debugWrite(self, content, channelName = "unsorted", location = "genericLocation"):
         debugDir = self.api.locations.getUserHomePath("debugWriter")
         subPath = os.path.join( os.path.abspath(debugDir), channelName )
@@ -154,34 +185,25 @@ class Utility(NameSpace):
         incrementStr = '{0:05d}'.format(increment)
         fnBin = os.path.join(subPath, "bin-" + incrementStr + "-" + location + ".bin")
         fnTxt = os.path.join(subPath, "textualContent.txt")
+        fnStack = os.path.join(subPath, "stack-" + incrementStr + "-" + location + ".txt")
 
         if self.isPy3:
             self._py3debugWrite(content, fnBin, fnTxt)
         else:
             self._py2debugWrite(content, fnBin, fnTxt)
 
+        with open(fnTxt, "a") as f:
+            f.write(str(increment))
+            f.write("\n")
+            val = self.getValueAsString(content, True)
+            f.write(val)
+            f.write("\n\n")
+
         fnDebug = os.path.join(subPath, "debugContent.txt")
         with open(fnDebug, "a") as f:
             f.write(str(increment))
             f.write("\n")
-
-            strtype = str(type(content))
-            if self.isPy3:
-                if isinstance(content, str):
-                    strtype = "str"
-                if isinstance(content, bytes):
-                    strtype = "str"
-            else:
-                if isinstance(content, str):
-                    strtype = "str"
-
-            if isinstance(content, int):
-                strtype = "int"
-
-            if isinstance(content, float):
-                strtype = "float"
-
-            f.write(strtype)
+            f.write(self.getTypeAsString(content))
             f.write("\n")
 
             stack = inspect.stack()
@@ -198,6 +220,26 @@ class Utility(NameSpace):
                     f.write("\n")
                 i = i - 1
 
+            f.write("\n")
+
+        with open(fnStack, "w") as f:
+            f.write(str(increment))
+            f.write("\n")
+
+            stack = inspect.stack()
+
+            i = len(stack) - 1
+
+            exclude = ["makehuman.py", "qtui.py", "qtgui.py", "mhmain.py"]
+
+            while i > 1:
+                (frame, filename, line_number, function_name, lines, index) = stack[i]
+                fn = os.path.basename(filename)
+                fn = fn.replace(".py", "")
+                if not fn in exclude:
+                    f.write(fn + "." + function_name + "():" + str(line_number))
+                    f.write("\n")
+                i = i - 1
             f.write("\n")
 
 
